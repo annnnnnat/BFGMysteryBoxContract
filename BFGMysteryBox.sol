e8f9a23f7b75b257e8da3c498b4cd85197d54fbc
annnnnnat <58486697+annnnnnat@users.noreply.github.com>
2022-12-23 21:00:13 +0100

    public+private+giveaway

--- BFGMysteryBox.sol
+++ BFGMysteryBox.sol
@@ -3,36 +3,51 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "erc721a/contracts/ERC721A.sol";
 
 contract BFGMysteryBox is ERC721A, Ownable {
+    using Counters for Counters.Counter;
     using SafeERC20 for IERC20;
+
+    Counters.Counter private _codeGenerator;
 
     IERC20 public immutable USD =
         IERC20(0x5425890298aed601595a70AB815c96711a31Bc65);
 
-    mapping(address => string) public code;
-    mapping(string => address) public codeAddress;
-    mapping(address => uint256) public points;
+    mapping(address => string) public affiliate_code;
+    mapping(string => address) public affiliate_codeAddress;
+    mapping(address => uint256) public affiliate_points;
 
-    address[] public arrayAddresses;
+    mapping(string => address) public giveaway_address;
+    mapping(address => string) public giveaway_code;
+    mapping(string => uint256) public giveaway_points;
+    mapping(address => bool) public hasMinted;
+
+    address[] public affiliate_addresses;
+    address[] public giveaway_addresses;
 
     uint256 public neededAvax;
     uint256 public neededUSD;
 
     uint256 public MAX_SUPPLY;
-    uint256 public reservedSupply;
+    uint256 public MAX_PRIVATE;
+
+    bool startPublic;
 
     receive() external payable {}
 
     constructor() ERC721A("BFG Mistery Box NFT", "BFG-MB") {
-        createPromoCode("0", owner());
         neededAvax = 13000000000000000000;
         neededUSD = 150000000;
         MAX_SUPPLY = 15000;
-        reservedSupply = 7500;
+        MAX_PRIVATE = 5000;
+        startPublic = false;
     }
 
     function compareStrings(string memory a, string memory b)
@@ -45,7 +60,7 @@ contract BFGMysteryBox is ERC721A, Ownable {
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmdUFuZmeKGit6kjuotbMEm1mzeh4Hg9am25ou3mJVXcUY/";
+        return "ipfs://QmNPMP1wYVNbYJnwSiDMbppwFFeTc8w4LhzyqXDgyjf7oz/";
     }
 
     function _startTokenId() internal view virtual override returns (uint256) {
@@ -68,67 +83,38 @@ contract BFGMysteryBox is ERC721A, Ownable {
                 : "";
     }
 
-    function _getAddresses() public view returns (address[] memory) {
-        return arrayAddresses;
+    function _getAffiliateAddresses() public view returns (address[] memory) {
+        return affiliate_addresses;
+    }
+
+    function _getGiveawayAddresses() public view returns (address[] memory) {
+        return giveaway_addresses;
     }
 
     //only Owner
-    function adaptAVAXPrice(uint256 price) public onlyOwner {
+    function changePublicMint() public onlyOwner {
+        startPublic = !startPublic;
+    }
+
+    //only Owner
+    function changeAVAXPrice(uint256 price) public onlyOwner {
         require(price > 0, "Price must be greater than 0");
 
         neededAvax = price;
     }
 
     //only Owner
-    function adaptUSDPrice(uint256 price) public onlyOwner {
+    function changeUSDPrice(uint256 price) public onlyOwner {
         require(price > 0, "Price must be greater than 0");
 
         neededUSD = price;
     }
 
     //only Owner
-    function setReservedSupply(uint256 newAmount) public onlyOwner {
-        reservedSupply = newAmount;
-    }
+    function changetMaxPrivate(uint256 _value) public onlyOwner {
+        require(_value > 0, "Amount must be greater than 0");
 
-    //only Owner
-    function createPromoCode(string memory _code, address _user)
-        public
-        onlyOwner
-    {
-        //check if code is taken
-        require(
-            codeAddress[_code] == address(0),
-            "Code is already used for an address"
-        );
-        //check if address already has a code
-        require(
-            compareStrings(code[_user], ""),
-            "You already have a code for this address"
-        );
-
-        codeAddress[_code] = _user;
-        code[_user] = _code;
-
-        arrayAddresses.push(_user);
-    }
-
-    function publicPromoCode(string memory _code) public {
-        require(!compareStrings(_code, ""), "Provide a valid code");
-        require(
-            codeAddress[_code] == address(0),
-            "Code is already used for an address"
-        );
-        //check if address already has a code
-        require(
-            compareStrings(code[msg.sender], ""),
-            "You already have a code for this address"
-        );
-
-        codeAddress[_code] = msg.sender;
-        code[msg.sender] = _code;
-
-        arrayAddresses.push(msg.sender);
+        MAX_PRIVATE = _value;
     }
 
     //only Owner
@@ -149,66 +135,85 @@ contract BFGMysteryBox is ERC721A, Ownable {
     }
 
     //only Owner
-    function mintReserved(
-        address player,
-        string memory _code,
-        uint256 quantity_
-    ) public onlyOwner {
-        uint256 newSupply = totalSupply() + quantity_;
-
+    function createAffiliateCode(string memory _code, address _user)
+        public
+        onlyOwner
+    {
+        //check if code is taken
         require(
-            reservedSupply >= quantity_,
-            "Quantity exceeds mintable reserves"
+            affiliate_codeAddress[_code] == address(0),
+            "Code is already used for an address"
         );
-        require(reservedSupply > 0, "No more mintable reserves");
-        require(newSupply <= MAX_SUPPLY, "Max supply reached");
+        //check if address already has a code
         require(
-            quantity_ > 0,
-            "Selected quantity of mint boxes must be greater than 0."
+            compareStrings(affiliate_code[_user], ""),
+            "You already have a code for this address"
         );
-        require(codeAddress[_code] != address(0), "Not a valid code.");
 
-        reservedSupply -= quantity_;
+        affiliate_codeAddress[_code] = _user;
+        affiliate_code[_user] = _code;
 
-        points[codeAddress[_code]] += quantity_;
-        _mintNFT(player, quantity_);
+        affiliate_addresses.push(_user);
     }
 
-    function mintBox(
-        string memory _code,
-        uint256 amount,
-        uint256 quantity_
-    ) public payable {
-        //is totalSupply reached
-        uint256 newSupply = totalSupply() + quantity_;
+    function createGiveawayCode(string memory _code) public {
+        require(
+            hasMinted[msg.sender] == true,
+            "Mint a mystery box to get a giveaway code."
+        );
+        //check if code is taken
+        require(
+            giveaway_address[_code] == address(0),
+            "Code is already used for an address"
+        );
+        //check if address already has a code
+        require(
+            compareStrings(giveaway_code[msg.sender], ""),
+            "You already have a code for this address"
+        );
+        giveaway_address[_code] = msg.sender;
+        giveaway_code[msg.sender] = _code;
+    }
 
+    function mintPrivateBox(
+        uint256 amount,
+        uint256 _quantity,
+        string memory _code
+    ) public payable {
+        uint256 newSupply = totalSupply() + _quantity;
+
+        require(!startPublic, "Public sale has started");
         require(newSupply <= MAX_SUPPLY, "Max supply reached");
         require(
-            newSupply <= (MAX_SUPPLY - reservedSupply),
-            "Remaining tokens are reserved"
+            newSupply <= MAX_PRIVATE,
+            "Max supply for private round reached"
         );
 
-        //check if more than one mint
+        //check the quantity sent
         require(
-            quantity_ > 0,
+            _quantity > 0,
             "Selected quantity of mint boxes must be greater than 0."
         );
 
-        //check if the code exists
-        require(codeAddress[_code] != address(0), "Not a valid code.");
-
-        //check if any payment is made
+        //check if any payment is sent
         require(msg.value > 0 || amount > 0, "Invalid payment attempt");
+
+        //check if valid code
+        require(
+            (giveaway_address[_code] != address(0)) ||
+                (affiliate_codeAddress[_code] != address(0)),
+            "Not a valid code"
+        );
 
         //USDT
         if (msg.value == 0 && amount > 0) {
-            uint256 calUSD = quantity_ * neededUSD;
-            //check if user has that much USDT
+            uint256 calUSD = _quantity * neededUSD;
+            //check if user has needed USDT
             require(
                 USD.balanceOf(msg.sender) >= calUSD,
                 "Insufficient balance of USDT"
             );
-            //check if value is that much USDT
+            //check if amount equals neededh USDT
             require(amount == calUSD, "Invalid payment attempt");
             //check if user payed
             require(
@@ -216,26 +221,117 @@ contract BFGMysteryBox is ERC721A, Ownable {
                 "transferFrom failed"
             );
 
-            points[codeAddress[_code]] += quantity_;
-            _mintNFT(msg.sender, quantity_);
+            _mintNFT(msg.sender, _quantity, _code);
         }
 
         //AVAX
         if (msg.value > 0 && amount == 0) {
-            uint256 calAVAX = quantity_ * neededAvax;
+            uint256 calAVAX = _quantity * neededAvax;
+            //check if user has needed AVAX
             require(
                 msg.sender.balance >= calAVAX,
                 "Insufficient balance of AVAX"
             );
+            //check if amount equals needed AVAX
             require(msg.value == calAVAX, "Invalid payment attempt");
 
-            points[codeAddress[_code]] += quantity_;
-
-            _mintNFT(msg.sender, quantity_);
+            _mintNFT(msg.sender, _quantity, _code);
         }
     }
 
-    function _mintNFT(address player, uint256 quantity_) internal {
+    function mintPublicBox(
+        uint256 amount,
+        uint256 _quantity,
+        string memory _code
+    ) public payable {
+        require(startPublic, "Public sale is not available at the moment");
+        uint256 newSupply = totalSupply() + _quantity;
+
+        require(newSupply <= MAX_SUPPLY, "Max supply reached");
+
+        //check the quantity sent
+        require(
+            _quantity > 0,
+            "Selected quantity of mint boxes must be greater than 0."
+        );
+
+        //check if any payment is sent
+        require(msg.value > 0 || amount > 0, "Invalid payment attempt");
+
+        //USDT
+        if (msg.value == 0 && amount > 0) {
+            uint256 calUSD = _quantity * neededUSD;
+            //check if user has needed USDT
+            require(
+                USD.balanceOf(msg.sender) >= calUSD,
+                "Insufficient balance of USDT"
+            );
+            //check if amount equals neededh USDT
+            require(amount == calUSD, "Invalid payment attempt");
+            //check if user payed
+            require(
+                USD.transferFrom(msg.sender, address(this), calUSD),
+                "transferFrom failed"
+            );
+
+            _mintNFT(msg.sender, _quantity, _code);
+        }
+
+        //AVAX
+        if (msg.value > 0 && amount == 0) {
+            uint256 calAVAX = _quantity * neededAvax;
+            //check if user has needed AVAX
+            require(
+                msg.sender.balance >= calAVAX,
+                "Insufficient balance of AVAX"
+            );
+            //check if amount equals needed AVAX
+            require(msg.value == calAVAX, "Invalid payment attempt");
+
+            _mintNFT(msg.sender, _quantity, _code);
+        }
+    }
+
+    function addressExists(address num) public view returns (bool) {
+        for (uint256 i = 0; i < giveaway_addresses.length; i++) {
+            if (giveaway_addresses[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function giveawayEnroll() public {
+        require(hasMinted[msg.sender], "You need to buy 1 box");
+        require(
+            giveaway_points[giveaway_code[msg.sender]] > 0,
+            "Use your promo code at least once"
+        );
+        require(!addressExists(msg.sender), "You have already enrolled");
+
+        giveaway_addresses.push(msg.sender);
+    }
+
+    function _mintNFT(
+        address player,
+        uint256 quantity_,
+        string memory _code
+    ) internal {
+        bool senderNoCode = compareStrings(giveaway_code[player], "");
+        //no giveaway points if user already has minted
+        if (giveaway_address[_code] != address(0)) {
+            if (senderNoCode) {
+                giveaway_points[_code] += 1;
+            }
+        }
+        //affiliate points
+        if (affiliate_codeAddress[_code] != address(0)) {
+            affiliate_points[affiliate_codeAddress[_code]] += quantity_;
+        }
+        //minted code
+        if (!hasMinted[player]) {
+            hasMinted[player] = true;
+        }
         _safeMint(player, quantity_);
     }
 }
